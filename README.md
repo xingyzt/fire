6c4f06e59ff3d2423561aed87146f52912a7e768
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2025-01-08 18:25:40 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+3.9 micron data from the GOES-16 East and GOES-18 West satellites on the January 2025 Southern California fires.
+We download the reprojected data from UW SSEC's [RealEarth program](https://realearth.ssec.wisc.edu/).
+Each image is 128x128 pixels, centered at (34.1, -118.4). Each pixel is 1.0 km.
---
+++ download.py
@@ -0,0 +1,41 @@
+import datetime
+import requests
+
+products = [
+    "G18-ABI-CONUS-BAND07", # GOES-16 West
+    "G16-ABI-FD-BAND07", # GOES-18 East
+]
+width = 128
+height = 128
+long = 34.1
+lat = -118.4
+zoom = 7
+baseurl = "http://realearth.ssec.wisc.edu/api/image"
+
+# UTC
+time_start = datetime.datetime(2025, 1, 7, 18, 16, 0, 0)
+time_end = datetime.datetime(2025, 1, 9, 0, 16, 0, 0)
+time_step = datetime.timedelta(minutes=5)
+
+time = time_start
+while time < time_end:
+    time += time_step
+    time_string = time.isoformat().replace("T","+")
+
+    for product in products:
+        url = f"{baseurl}?products={product},&width={width}&height={height}&client=RealEarth&basemap=-&labels=-&center={long},{lat}&zoom={zoom}&time={time_string}"
+        path = f"raw/{product}/{time}.png"
+
+        with open(path, 'wb') as handle:
+            response = requests.get(url, stream=True)
+
+            if not response.ok:
+                print(response)
+
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+
+                handle.write(block)
+
+    print(time_string)
